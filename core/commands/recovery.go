a54eb29e597ffe0c51d03b4df6f25839ee908b6d
gov <govi218mu@gmail.com>
2020-08-17 10:23:09 -0400

    added encode trategy option

--- core/commands/recovery.go
+++ core/commands/recovery.go
@@ -5,6 +5,7 @@ import (
 	"io"
 
 	recovery "github.com/Wondertan/go-ipfs-recovery"
+	"github.com/Wondertan/go-ipfs-recovery/entanglement"
 	"github.com/Wondertan/go-ipfs-recovery/reedsolomon"
 	"github.com/ipfs/go-cid"
 	cmds "github.com/ipfs/go-ipfs-cmds"
@@ -20,6 +21,9 @@ type EncodeResp struct {
 
 const (
 	recoverabilityOptionName = "recoverability"
+	encodeStrategyOptionName = "encode-strategy"
+	rs                       = "reed-solomon"
+	ent                      = "entangle"
 )
 
 var RecoveryCmd = &cmds.Command{
@@ -33,9 +37,12 @@ var encodeCmd = &cmds.Command{
 		cmds.StringArg("path", true, false, "The path to the IPFS object(s) to be encoded.").EnableStdin(),
 	},
 	Options: []cmds.Option{
-		cmds.IntOption("recoverability", "r", "Recoverability for the DAG to be encoded with.").WithDefault(3),
+		cmds.IntOption(recoverabilityOptionName, "r", "Recoverability for the DAG to be encoded with.").WithDefault(3),
+		cmds.StringOption(encodeStrategyOptionName, "s", "Choice of erasure coding algorithm.").WithDefault(rs),
 	},
 	Run: func(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment) error {
+		var renc recovery.Encoder
+
 		nd, err := cmdenv.GetNode(env)
 		if err != nil {
 			return err
@@ -53,7 +60,19 @@ var encodeCmd = &cmds.Command{
 		}
 
 		r, _ := req.Options[recoverabilityOptionName].(int)
-		enc, err := recovery.EncodeDAG(req.Context, nd.DAG, reedsolomon.NewEncoder(nd.DAG), rnd, r)
+
+		s, _ := req.Options[encodeStrategyOptionName].(string)
+
+		switch s {
+		case rs:
+			renc = reedsolomon.NewEncoder(nd.DAG)
+		case ent:
+			renc = entanglement.NewEncoder(nd.DAG)
+		default:
+			err = fmt.Errorf("Invalid recovery strategy")
+		}
+
+		enc, err := recovery.EncodeDAG(req.Context, nd.DAG, renc, rnd, r)
 		if err != nil {
 			return err
 		}
